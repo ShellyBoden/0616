f07ce60d61024a563c4d53545534df26e76761a5
ShellyBoden <43201167+ShellyBoden@users.noreply.github.com>
2019-06-16 10:51:36 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <MyNav></MyNav>
+    <BigSection></BigSection>
+    <ShopList></ShopList>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ShopList from './components/ShopList.vue'
+import MyNav from './components/MyNav.vue'
+import BigSection from './components/BigSection.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    ShopList,
+    MyNav,
+    BigSection
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: '微軟正黑體';
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  /* color: #2c3e50; */
+  margin: 0;
+  padding: 0;
+  /* margin-top: 60px; */
+}
+</style>
---
+++ src/components/MyNav.vue
@@ -0,0 +1,65 @@
+<template>
+<div class="container">
+      <ul>comment</ul>
+      <ul>information</ul>
+      <ul>goods</ul>
+      <img src="../assets/ICON.png">
+      <ul>contact</ul>
+      <ul>love</ul>
+      <ul>shop cart</ul>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.container{
+    width: 100%;
+    height: 25vh;
+    background-color: #f88e7b;
+    display: flex;
+    justify-content:space-around;
+    align-items:center;
+}
+
+ul{
+    list-style-type: none;
+    margin: 4vw;
+    padding: 0;
+    font-family: 'Baskerville';
+}
+
+img{
+    width: 5%;
+}
+
+/* -------------------rwd------------------- */
+
+@media screen and (max-width: 400px){
+.container{
+    width: 100%;
+    height: 10vh;
+    background-color: #f88e7b;
+    display: flex;
+    justify-content:space-around;
+    align-items:center;
+}
+
+ul{
+    list-style-type: none;
+    padding: 0;
+    font-family: 'Baskerville';
+    font-size: 8px;
+}
+
+img{
+    width: 10%;
+}
+
+
+}
+</style>
